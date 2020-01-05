03f4405313888c80e201e0d0daedae35816c5dd6
psyssai <psyssai@gmail.com>
2020-01-05 21:13:01 +0900

    1.3 prettier 설정 추가

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Alert} from "react-native"
+import React from "react";
+import { Alert } from "react-native";
 import Loading from "./Loading";
 import * as Location from "expo-location";
 import axios from "axios";
@@ -9,25 +9,28 @@ export default class App extends React.Component {
   state = {
     isLoading: true
   };
-  getWeather = async(latitude, longitude) => {
-    const {data} = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`);
+  getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}`
+    );
     console.log(data);
-  }
-
+  };
   getLocation = async () => {
     try {
       await Location.requestPermissionsAsync();
-      const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync();
+      const {
+        coords: { latitude, longitude }
+      } = await Location.getCurrentPositionAsync();
       this.getWeather(latitude, longitude);
-      this.setState({ isLoading: false});
-    } catch (error){
+      this.setState({ isLoading: false });
+    } catch (error) {
       Alert.alert("Can't find you.", "So sad");
     }
-  }
-  componentDidMount(){
+  };
+  componentDidMount() {
     this.getLocation();
   }
-  render(){
+  render() {
     const { isLoading } = this.state;
     return isLoading ? <Loading /> : null;
   }
